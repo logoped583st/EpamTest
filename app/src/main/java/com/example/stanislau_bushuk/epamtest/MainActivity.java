e7b867f2cf7d211a9cae7517ad7d7bf95a6eba73
logoped583st <logoped583st@mail.ru>
2018-03-23 12:23:42 +0300

    fake

--- app/src/main/java/com/example/stanislau_bushuk/epamtest/MainActivity.java
+++ app/src/main/java/com/example/stanislau_bushuk/epamtest/MainActivity.java
@@ -73,6 +73,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
+        }else{
+            finish();
         }
     }
 
